b1aa1810b5e6a3a48cc2123edaa633d1b02ed221
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2018-03-14 13:32:10 +0100

    added script and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simple rsync backup
+
+This repository contains a very basic rsync backup script. The bash script is
+mostly used to backup data on Windows using Cygwin.
+
+## Usage
+
+The script expects a `backup.conf` in its directory. The configuration file
+itself is a Bash script as well that stores the source directories for backup in
+an array variable named `src`. The destination directory is specified by the
+variable `dst`. Additionally, one may provide the name of the file containing a
+list of excluded files which is passed to `rsync` using the `--exclude-from`
+parameter.
+
+A typical `backup.conf` looks as follows:
+
+```bash
+src[0]=/cygdrive/d/Data1
+src[1]=/cygdrive/d/Data2
+dst=/cygdrive/e/Backup
+exclude=excluded.txt
+```
---
+++ backup.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+[[ -f ./backup.conf ]] || \
+  { echo error: missing backup.conf >&2; exit 1; }
+
+source ./backup.conf
+
+# The folder name of the current backup
+readonly FOLDER=$(date "+%Y-%m-%d %H_%M_%S")
+# The path to the current backup
+TARGET=$dst/$FOLDER
+readonly LOG_FILE="$dst/$FOLDER.log"
+
+# The symbolic link to the last backup
+readonly LAST_TARGET=$dst/.last-backup
+readonly LAST_ABORTED_TARGET=$dst/.aborted-backup
+
+# TODO check backup-exclude.txt
+
+# TODO use absolute paths
+RSYNCCONFIG=(
+  -a
+  --info=progress2
+  --delete-delay
+  --delete-excluded
+  --chmod=ugo=rwX
+  #--no-g
+  #--no-p
+  --stats
+  --log-file="$LOG_FILE"
+)
+
+# TODO check exclude if provided
+if [[ -s $exclude ]]
+then
+  RSYNCCONFIG+=(--exclude-from="$exclude")
+fi
+
+if [ -L "$LAST_ABORTED_TARGET" ]
+then
+  LAST_TARGET_RESOLVED=$(realpath -P "$LAST_TARGET")
+  LAST_TARGET_REAL=$(realpath -P "$LAST_ABORTED_TARGET")
+  # rsync failed: create a symlink to the last target
+  echo "detected an earlier started backup"
+  echo "resuming to backup from $LAST_TARGET_RESOLVED to $LAST_TARGET_REAL"
+
+  TARGET=$LAST_TARGET_REAL
+  RSYNCCONFIG+=(--link-dest="$LAST_TARGET_RESOLVED")
+elif [ -d "$LAST_TARGET" ]
+then
+  RSYNCCONFIG+=(--link-dest="$LAST_TARGET")
+fi
+
+# TODO unlink only if the TARGET exists (no dry-run)
+rsync "${RSYNCCONFIG[@]}" "${src[@]}" "$TARGET"
+
+if [ $? -ne 0 ]
+then
+  if [ -L "$LAST_ABORTED_TARGET" ]
+  then
+    unlink "$LAST_ABORTED_TARGET"
+  fi
+
+  # Do not remove .last-backup if exists
+  ln -s "$TARGET" "$LAST_ABORTED_TARGET"
+
+  if [ $? -eq 0 ]
+  then
+    echo "backup aborted: you can resume the backup later"
+  else
+    echo "backup aborted: failed to create a check point. \
+                          backup cannot be resumed."
+  fi
+else
+  # In case rsync succeeded, create a symbolic link to the last target that will
+  # be used as argument for --link-dest
+  if [ -d "$TARGET" ]
+  then
+  ( unlink "$LAST_TARGET" 2>/dev/null; exit 0 ) && \
+    ln -s "$TARGET" "$LAST_TARGET"
+  fi
+
+  # Since rsync finished successfully, remove the symlink to the last aborted
+  # target.
+  if [ -L "$LAST_ABORTED_TARGET" ]
+  then
+    unlink "$LAST_ABORTED_TARGET"
+  fi
+fi
